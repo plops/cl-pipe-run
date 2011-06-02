71df418fad3661bf5f7d2567cb8d8014471c931e
martin <martin@hop.kcl.ac.uk>
2011-06-02 17:29:29 +0100

    strtok

--- cmd.c
+++ cmd.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
+#include <string.h>
+
+// this is not threadsafe, I use strtok
 
 enum{CMDLEN=100};
 
-unsigned long
-hash(char*s)
-{ // djb2
-  unsigned long hash=5381;
-  int c;
-  while((c=*s++))
-    hash=(33*hash)^c;
-  return hash;
-}
-
-// transform first strong into hash
+// string containing function name
 // optionally read an ascii number q
 // followed by \n
 // then come q bytes of raw data
@@ -20,10 +13,18 @@ hash(char*s)
 int
 main()
 {
-  char s[CMDLEN],*p;
+  char s[CMDLEN],*line;
   do{
-    p=fgets(s,sizeof(s),stdin);
-    printf("hash %lu: \"%s\"\n",hash(s),s);
-  }while(p);
+    line=fgets(s,sizeof(s),stdin);
+    char *search=" ",*tok=0;
+    if(line){
+      tok=strtok(line,search);
+      while(tok!=0){
+	printf("+%s+\n",tok);
+	tok=strtok(0,search);
+      }
+      printf("\n");
+    }    
+  }while(line);
   return 0;
 }
