7517d7f93a4426e284a04074088986a89ccf64e6
martin <martin@hop.kcl.ac.uk>
2011-06-02 17:50:38 +0100

    split into either function name or float number

--- cmd.c
+++ cmd.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 // this is not threadsafe, I use strtok
+
+#define len(x) (sizeof(x)/sizeof(x[0]))
 
 enum{CMDLEN=100};
 
@@ -11,16 +15,48 @@ enum{CMDLEN=100};
 // then come q bytes of raw data
 
 int
+fun(int a)
+{
+  return a+a;
+}
+
+int
+fub(int a)
+{
+  return 3*a;
+}
+
+
+struct{
+  char name[CMDLEN];
+  int (*fptr)();
+}cmd[]={{"fun",fun},
+	{"fub",fub}};
+
+int
+lookup(char*s)
+{
+  unsigned int i;
+  for(i=0;i<len(cmd);i++)
+    if(0==strncmp(s,cmd[i].name,CMDLEN))
+      return i;
+  return -1;
+}
+
+int
 main()
 {
   char s[CMDLEN],*line;
   do{
     line=fgets(s,sizeof(s),stdin);
-    char *search=" ",*tok=0;
+    char *search=" ",*tok;
     if(line){
       tok=strtok(line,search);
       while(tok!=0){
-	printf("+%s+\n",tok);
+	if(isalpha(tok[0]))
+	  printf("+%s=%d+\n",tok,lookup(tok));
+	else
+	  printf("+%s=%g+\n",tok,atof(tok));
 	tok=strtok(0,search);
       }
       printf("\n");
