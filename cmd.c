f56b6d7d5aa87739b68a24a9f41818a12dec457d
martin <martin@hop.kcl.ac.uk>
2011-06-02 18:03:32 +0100

    parses function name followed by floats

--- cmd.c
+++ cmd.c
@@ -1,9 +1,9 @@
+// this is not threadsafe, I use strtok
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-// this is not threadsafe, I use strtok
 
 #define len(x) (sizeof(x)/sizeof(x[0]))
 
@@ -13,7 +13,15 @@ enum{CMDLEN=100};
 // optionally read an ascii number q
 // followed by \n
 // then come q bytes of raw data
-
+/*
+./cmd << EOF
+fun 3.1415 2.8
+fub -2.34
+3 fub a
+fub fun b
+fub .d3s0
+EOF
+*/
 int
 fun(int a)
 {
@@ -25,7 +33,6 @@ fub(int a)
 {
   return 3*a;
 }
-
 
 struct{
   char name[CMDLEN];
@@ -43,6 +50,13 @@ lookup(char*s)
   return -1;
 }
 
+int isfloatchar(int c)
+{
+  if(c=='+'||c=='-'||c=='.'||isdigit(c))
+    return 1;
+  return 0;
+}
+
 int
 main()
 {
@@ -52,12 +66,25 @@ main()
     char *search=" ",*tok;
     if(line){
       tok=strtok(line,search);
+      int i=0;
       while(tok!=0){
-	if(isalpha(tok[0]))
-	  printf("+%s=%d+\n",tok,lookup(tok));
+	if(i==0)
+	  if(isalpha(tok[0]))
+	    printf("+%s=%d+\n",tok,lookup(tok));
+	  else
+	    printf("error, expected function name\n");
 	else
-	  printf("+%s=%g+\n",tok,atof(tok));
+	  if(isfloatchar(tok[0])){
+	    char*endptr;
+	    double d=strtod(tok,&endptr);
+	    if(endptr==tok)
+	      printf("error, couldn't parse double\n");
+	    else
+	      printf("+%s=%g+\n",tok,d);
+	  }else
+	    printf("error, expected digit or .+- but found %c\n",tok[0]);
 	tok=strtok(0,search);
+	i++;
       }
       printf("\n");
     }    
