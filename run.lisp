2cd4d3f0d62708fb0289f6cbc43e5b1024d22ac2
martin <martin@hop.kcl.ac.uk>
2011-06-07 15:38:56 +0100

    cmd.c

--- run.lisp
+++ run.lisp
@@ -3,34 +3,31 @@
   (sb-ext:run-program "./cmd" '()
 		      :output :stream
 		      :input :stream
-		      :error :stream
 		      :wait nil))
 
-(let ((s (sb-ext:process-input *chan*)))
-  (write-line "fun 4" s)
-  (finish-output s))
+(defun cmd (cmd)
+ (let ((s (sb-ext:process-input *chan*)))
+   (write-line cmd s)
+   (finish-output s)))
+#+nil
+(cmd "fun 3")
 
+(defun send-binary ()
+ (with-open-file (e "binary" :direction :output
+		    :if-exists :append
+		    :if-does-not-exist :error
+		    :element-type '(unsigned-byte 8))
+   (let* ((s (sb-ext:process-input *chan*))
+	  (n 4)
+	  (buf (make-array n :element-type '(unsigned-byte 8))))
+     (setf (aref buf 0) 12)
+     (write-line (format nil "get ~a" n) s)
+     (finish-output s)
+     (write-sequence buf e)
+     (finish-output e))))
+#+nil
+(send-binary)
 
-(let* ((s (sb-ext:process-input *chan*))
-       (e (sb-ext:process-error *chan*))
-       (n 4)
-       (buf (make-array n :element-type '(unsigned-byte 8))))
-  (write-line (format nil "get ~a" n) s)
-  (finish-output s)
-  (write-sequence buf e)
-  (finish-output e))
-
-(progn
- (defvar *dump* nil)
- (sb-thread:make-thread 
-  #'(lambda ()
-      (with-open-stream (s (sb-ext:process-error *chan*))
-	(loop
-	   (let ((a (make-array 4 :element-type '(unsigned-byte 8))))
-	     (read-sequence a s)
-	     (format t "dump ~a.~%" a)
-	     (setf *dump* a)))))
-  :name "dump-reader"))
 
 (sb-thread:make-thread 
  #'(lambda ()
