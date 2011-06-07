b49dd963870d4628a0dbd74e3de6734b7567d76f
martin <martin@hop.kcl.ac.uk>
2011-06-07 09:26:17 +0100

    lisp reads raw dump on stderr

--- run.lisp
+++ run.lisp
@@ -3,11 +3,24 @@
   (sb-ext:run-program "./cmd" '()
 		      :output :stream
 		      :input :stream
+		      :error :stream
 		      :wait nil))
 
 (let ((s (sb-ext:process-input *chan*)))
-  (write-line "fun 3" s)
+  (write-line "dump 4" s)
   (finish-output s))
+
+(progn
+ (defvar *dump* nil)
+ (sb-thread:make-thread 
+  #'(lambda ()
+      (with-open-stream (s (sb-ext:process-error *chan*))
+	(loop
+	   (let ((a (make-array 4 :element-type '(unsigned-byte 8))))
+	     (read-sequence a s)
+	     (format t "dump ~a.~%" a)
+	     (setf *dump* a)))))
+  :name "dump-reader"))
 
 (sb-thread:make-thread 
  #'(lambda ()
